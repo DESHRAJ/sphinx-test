6f4ead039f027ddfa01b4c6b6c80a3d514b6ed6d
Deshraj <deshrajdry@gmail.com>
2015-05-20 16:02:48 +0530

    rebuilt docs through sphinx

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:46,filenames:["includeme","index"],objects:{},objnames:{},objtypes:{},terms:{content:1,index:1,modul:1,page:1,search:1},titles:["&lt;no title&gt;","Welcome to sphinxtest&#8217;s documentation!"],titleterms:{document:1,indic:1,sphinxtest:1,tabl:1,welcom:1}})
